44e00dc4866c25da2651bb725e3c8712409ec416
Raghav Khatoria <raghavkhatoria42@gmail.com>
2021-06-06 10:13:04 +0530

    Deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const path = require("path");
 const app = express();
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
@@ -12,6 +13,12 @@ connectDB();
 
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('../client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+}
 app.get("/", (req, res, next) => {
   res.send("Api running");
 });
