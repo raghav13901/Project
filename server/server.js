21493556691cb328090e3fb529878fc1aa662bea
Raghav Khatoria <raghavkhatoria42@gmail.com>
2021-06-06 08:15:57 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const app = express();
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
 connectDB();
 
 app.use(express.json());
